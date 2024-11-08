e59ffdb3e20b396fc4edf2f311a29c8bf717ceb1
Paul V Nguyen <paulngyn512@gmail.com>
2024-11-08 00:06:13 -0800

    test2

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <NavBar setPage={setPage} />
-      {page === 'logo' && <h1>Welcome to our Produc WebApp</h1>}
+      {page === 'logo' && <h1>Welcome to our Product WebApp</h1>}
       {page === 'product' && <h1>Product Page</h1>}
       {page === 'about' && <h1>About Us Page</h1>}
       {page === 'contact' && <h1>Contact Us Page</h1>}
