46f8efc9f532dbcd455e09de780fd082fef73d36
Ron <ronelijah28@gmail.com>
2025-01-15 14:43:30 -0800

    more popup

--- app/components/QuoteReqButton/QuoteReqButon.tsx
+++ app/components/QuoteReqButton/QuoteReqButon.tsx
@@ -1,17 +1,34 @@
 "use client";
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './QuoteReqButton.module.css';
+import QuoteForm from '../QuoteForm/QuoteForm';
 
 interface QuoteReqButtonProps {
   setPage: (page: string) => void;
 }
 
 const QuoteReqButton: React.FC<QuoteReqButtonProps> = ({ setPage }) => {
-
+  const [showModal, setShowModal] = useState(false);
+  const handleOpenModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
   return (
-    <button onClick={() => setPage('quote')} className={styles.navButton}>
+    <div>
+    <button onClick={handleOpenModal} className={styles.navButton}>
       Get A Quote
     </button>
+    
+    {showModal && (
+      <div className={styles.modalOverlay}>
+        <div className={styles.modalContent}>
+          <QuoteForm onClose={handleCloseModal} />
+        </div>
+        <div className={styles.modalBackdrop} onClick={handleCloseModal}></div>
+      </div>
+    )}
+  </div>
+
+    
+    
   );
 };
 
