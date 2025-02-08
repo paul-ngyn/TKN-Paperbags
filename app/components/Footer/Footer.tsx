991bce986e1e945cf4394b31eaeb55d0327bbbac
Paul V Nguyen <paulngyn512@gmail.com>
2025-02-07 16:22:41 -0800

    footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from 'react';
+import Link from 'next/link';
 import styles from './Footer.module.css';
 import Image from 'next/image';
 
@@ -10,8 +11,21 @@ import mtcLogo from '../../public/MTC-logo.png';
 const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.logoContainer}>
-        <Image src={mtcLogo} alt="MTC Logo" width={150} height={150} />
+      <div className={styles.logoInfoWrapper}>
+        <div className={styles.logoContainer}>
+          <Image src={mtcLogo} alt="MTC Logo" width={150} height={150} />
+        </div>
+        <div className={styles.contactInfo}>
+          <p>Address: 123 Maple Street, City, Country</p>
+          <p>Phone: 777-777-7777</p>
+          <p>Email: info@mapletradecorp.com</p>
+        </div>
+      </div>
+      <div className={styles.linksContainer}>
+        <Link href="/bags" className={styles.footerLink}>Bags</Link>
+        <Link href="/about" className={styles.footerLink}>About Us</Link>
+        <Link href="/contact" className={styles.footerLink}>Contact Us</Link>
+        <Link href="/orderinfo" className={styles.footerLink}>Ordering Information</Link>
       </div>
       <p className={styles.footerText}>© 2025 Maple Trade Corporation. All rights reserved.</p>
     </footer>
