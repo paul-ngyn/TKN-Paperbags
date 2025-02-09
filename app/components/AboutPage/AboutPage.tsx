9e94da7e6807cd3d95adcc2077810f298df31fb6
Paul V Nguyen <paulngyn512@gmail.com>
2025-02-09 13:26:13 -0800

    ABOUT US PAGE MASS CHANGES SUPERBOWL SUNDAY GO CHIEFS 3-PEAT BABY GO MAHOMES

--- app/components/AboutPage/AboutPage.tsx
+++ app/components/AboutPage/AboutPage.tsx
@@ -13,32 +13,35 @@ interface AboutPageProps {
 const AboutPage: React.FC<AboutPageProps> = ({ handleNavigation }) => {
   return (
     <div className={styles.container}>
-      {/* Section 1: MTC logo on the left, text on the right */}
+      {/* MTC logo on the left, text on the right */}
       <div className={styles.section}>
         <div className={styles.logoTextRow}>
           <div className={styles.logoContainer}>
             <Image
               src={mtcLogo}
               alt="MTC Logo"
-              width={150}
-              height={150}
+              width={200}
+              height={200}
               className={styles.fadeIn}
             />
           </div>
-          <div className={styles.textContainer}>
-            <h2>MTC - About Us</h2>
+          <div className={`${styles.textContainer} ${styles.fadeIn}`}>
+          <h2 style={{ fontWeight: "bold" }}>MAPLE TRADE CORPORATION (MTC)</h2>
             <p>
-              Learn about Maple Trade Corporation’s commitment to quality,
-              sustainability, and innovative solutions delivered with integrity.
+            MTC was founded in 2006 in the heart of San Francisco and in 2019 we expanded our operations and facilities to Hayward, CA.
+            <br />
+            Our goal is to provide the highest quality of eco-friendly, biodegradable, and compostable products seen in the world today.
+            <br />
+            With more than 10+ years of experience in the plastic container business, we take pride in our products and service and are honored to provide solutions for food service professionals across North America. 
             </p>
           </div>
         </div>
       </div>
-      {/* Section 2: TKN logo on the right, text on the left */}
+      {/* logo on the right, text on the left */}
       <div className={styles.section}>
-        <div className={styles.logoTextRowReverse}>
-          <div className={styles.textContainer}>
-            <h2>TKN - About Us</h2>
+        <div className={styles.logoTextRow}>
+          <div className={`${styles.textContainer} ${styles.fadeIn}`}>
+          <h2 style={{ fontWeight: "bold" }}>TKN - OUR BRAND</h2>
             <p>
               Discover our TKN products and how our expertise ensures seamless integration
               of cutting‑edge designs with practicality.
@@ -48,8 +51,8 @@ const AboutPage: React.FC<AboutPageProps> = ({ handleNavigation }) => {
             <Image
               src={tknLogo}
               alt="TKN Logo"
-              width={150}
-              height={150}
+              width={200}
+              height={200}
               className={styles.fadeIn}
             />
           </div>
