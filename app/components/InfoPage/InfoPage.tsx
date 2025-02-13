8a9e220ab63a226b69485db561ff8e572b1f3519
Paul V Nguyen <paulngyn512@gmail.com>
2025-02-13 15:57:49 -0800

    formatting for order info page

--- app/components/InfoPage/InfoPage.tsx
+++ app/components/InfoPage/InfoPage.tsx
@@ -1,7 +1,10 @@
 "use client";
 
-import React from 'react';
-import styles from './InfoPage.module.css';
+import React from "react";
+import Image from "next/image";
+import styles from "./InfoPage.module.css";
+import placeholder from "../../public/placeholder.png";
+import servicepic from "../../public/mtc_history-1.jpg"
 
 interface InfoPageProps {
   handleNavigation?: (page: string) => void;
@@ -10,17 +13,42 @@ interface InfoPageProps {
 const InfoPage: React.FC<InfoPageProps> = ({ handleNavigation }) => {
   return (
     <div className={styles.container}>
-      <div className={styles.section}>
-        <h2>Image Upload Details</h2>
-        <p>
-          Here you can find the details on how to upload images for your orders. Make sure your images meet the required specifications for the best results.
-        </p>
+      {/* Section 1: Text on the left, image on the right */}
+      <div className={`${styles.section} ${styles.row}`}>
+        <div className={styles.textContainer}>
+          <h2>Image Upload Details</h2>
+          <p>
+            Here you can find the details on how to upload images for your orders.
+            Make sure your images meet the required specifications for the best results.
+          </p>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src={placeholder}
+            alt="Placeholder"
+            width={300}
+            height={300}
+          />
+        </div>
       </div>
-      <div className={styles.section}>
-        <h2>Order Process</h2>
-        <p>
-          Learn about our order process, from placing an order to delivery. We ensure a smooth and efficient process to meet your needs promptly.
-        </p>
+
+      {/* Section 2: Image on the left, text on the right */}
+      <div className={`${styles.section} ${styles.rowreverse}`}>
+        <div className={styles.imageContainer}>
+          <Image
+            src={servicepic}
+            alt="Service"
+            width={500}
+            height={500}
+          />
+        </div>
+        <div className={styles.textContainer}>
+          <h2>Order Process</h2>
+          <p>
+            Learn about our order process, from placing an order to delivery.
+            We ensure a smooth and efficient process to meet your needs promptly.
+          </p>
+        </div>
       </div>
     </div>
   );
