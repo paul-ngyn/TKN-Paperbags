aadf6f54032f7b68c833a603d42ffedb0aba0f8f
Paul V Nguyen <paulngyn512@gmail.com>
2025-06-09 16:50:44 -0700

    added bg remover library

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import Image from "next/image"; // This is Next.js Image component
+import Image from "next/image"; 
 import Link from "next/link";
 import styles from "./Sidebar.module.css";
 import downloadicon from "../../public/downloadicon.png";
